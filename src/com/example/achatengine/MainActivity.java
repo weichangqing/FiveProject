8ecc816f5f0c11ef1335a6dd5fc3778e301e76e8
weichangqing <wei.changfa@etaoshi.com>
2014-11-19 11:52:51 +0800

    Update MainActivity.java

--- src/com/example/achatengine/MainActivity.java
+++ src/com/example/achatengine/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends Activity implements OnClickListener {
             isSingleView = true;
             layoutViewContent.removeAllViews();
             view = new BarChartView(MainActivity.this, isSingleView);
-            view.initData(first, second, options, "单柱状图");
+            view.initData(first, second, options, "444444");
             layoutViewContent.addView(view.getBarChartView());
             break;
         case R.id.double_view_btn:
